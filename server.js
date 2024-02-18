9b31f780deb2bbad375296dfa96cf5e9c9cdc16d
shareekaepps <shareekaepps@icloud.com>
2024-02-17 20:10:39 -0500

    .env added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require('./app');
+const app = require("./app");
 
 // TODO: Read the port from environment variables,
 // or use 9000 as a default
 const PORT = 9000;
 
-app.listen(PORT, console.log(`Listening on port ${PORT}...`));
+app.listen(PORT, console.log(`💎💎💎Listening on port ${PORT}...💎💎💎`));
